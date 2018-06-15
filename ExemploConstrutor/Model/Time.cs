5b1b68588c784241340df6849cad502a8c848a52
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-06-15 17:13:39 -0300

    Exemplo pronto

---
+++ ExemploConstrutor/Model/Time.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Time
+    {
+        //prop tab tab
+        public string Nome {get; set;}
+        public int QuantidadeGolsFeito { get; set; }
+        public string Pais { get; set; }
+        public int QuantidadeGolsSofridos { get; set; }
+        public int QuantidadesCartaoAmarelo { get; set; }
+        public int QuantidadesCartaoVermelho { get; set; }
+        public int QuantidadesDeSubstituicoes { get; set; }
+        public int QuantidadesFinais { get; set; }
+        public int QuantidadeVitorias { get; set; }
+        public int QuantidadeDerrotas { get; set; }
+        public decimal Faturamento { get; set; }
+        public string Serie { get; set; }
+
+        public Time(string nome, string pais, string serie)
+        {
+            Nome = nome;
+            Pais = pais;
+            Serie = serie;
+        }
+
+        public Time(string nome, string pais) // dois construtores nao podem ter os mesmo tipos string string
+        {
+            Nome = nome;
+            Pais = pais;
+        }
+
+
+    }
+}
---
+++ ExemploConstrutor/WFA/CadastrarTimes.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WFA
+{
+    partial class CadastrarTimes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFaturamento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCadastroRapidoPais = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCadastroRapidoNome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(35, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtNome.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtNome.Location = new System.Drawing.Point(116, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(249, 30);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtPais
+            // 
+            this.txtPais.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtPais.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPais.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtPais.Location = new System.Drawing.Point(116, 92);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(249, 30);
+            this.txtPais.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(35, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pais";
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtSerie.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSerie.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtSerie.Location = new System.Drawing.Point(116, 154);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(249, 30);
+            this.txtSerie.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(35, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Serie";
+            // 
+            // txtFaturamento
+            // 
+            this.txtFaturamento.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtFaturamento.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFaturamento.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtFaturamento.Location = new System.Drawing.Point(553, 27);
+            this.txtFaturamento.Name = "txtFaturamento";
+            this.txtFaturamento.Size = new System.Drawing.Size(249, 30);
+            this.txtFaturamento.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(404, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Faturamento";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(492, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 92);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCadastroRapidoPais
+            // 
+            this.txtCadastroRapidoPais.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtCadastroRapidoPais.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCadastroRapidoPais.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtCadastroRapidoPais.Location = new System.Drawing.Point(322, 404);
+            this.txtCadastroRapidoPais.Name = "txtCadastroRapidoPais";
+            this.txtCadastroRapidoPais.Size = new System.Drawing.Size(249, 30);
+            this.txtCadastroRapidoPais.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(241, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 23);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Pais";
+            // 
+            // txtCadastroRapidoNome
+            // 
+            this.txtCadastroRapidoNome.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtCadastroRapidoNome.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCadastroRapidoNome.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtCadastroRapidoNome.Location = new System.Drawing.Point(322, 339);
+            this.txtCadastroRapidoNome.Name = "txtCadastroRapidoNome";
+            this.txtCadastroRapidoNome.Size = new System.Drawing.Size(249, 30);
+            this.txtCadastroRapidoNome.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(241, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Nome";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(336, 460);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 92);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "CADASTRO RAPIDO";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CadastrarTimes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(887, 564);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtCadastroRapidoPais);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCadastroRapidoNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFaturamento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.Name = "CadastrarTimes";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFaturamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCadastroRapidoPais;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCadastroRapidoNome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
